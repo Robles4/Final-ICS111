1acdc13f76543bfab642b6eb09f917b87fbde0eb
Robles4 <164675622+Robles4@users.noreply.github.com>
2024-04-12 13:14:29 -1000

    Me idiot forgot this app

---
+++ Final.py
@@ -0,0 +1,35 @@
+from tkinter import*
+import customtkinter 
+from PIL import ImageTk, Image
+import pygame
+import sys
+
+def pygame_start():
+    app.destroy()
+    print("Test")
+
+
+def button_callback():
+    print("Button Pressed")
+
+app = customtkinter.CTk()
+app.title("My first test")
+app.geometry("400x140")
+app.grid_columnconfigure(0, weight=1)
+
+button = customtkinter.CTkButton(app, text="My first test", command=button_callback)
+button.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
+
+
+
+app.mainloop()
+
+pygame.init()
+
+wind=pygame.display.set_mode((750,650))
+pygame.display.set_caption("Paint Rush")
+while True:
+         for eve in pygame.event.get():
+              if eve.type==pygame.QUIT:
+                   pygame.quit()
+                   sys.exit()
