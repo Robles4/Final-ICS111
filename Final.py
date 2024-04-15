aebb3b3fd68cad3c14dfa607f2e457e3f681b16c
Robles4 <164675622+Robles4@users.noreply.github.com>
2024-04-15 13:26:46 -1000

    Update Final.py
    4.15.2024

--- Final.py
+++ Final.py
@@ -1,59 +1,194 @@
-from tkinter import*
+from PIL import ImageTk, Image
 import tkinter as tk
 from tkinter import ttk
-from PIL import ImageTk, Image
-import pygame
-import sys
-
-def pygame_start():
-    root.destroy()
-    print("Test")
 
 
-root=Tk()
-text=Text(root, height=8)
-text.pack
-#photo = Image.open("Final-Project/Assets/BackgroundImage.png")
-photo = ImageTk.PhotoImage(Image.open("Final-ICS111/Assets/Canvas.png").resize((1750, 1750)))
-w = Label(root, image=photo)
-w.pack()
-root.title('Paint Rush')
-root['bg']='black'
-def tab():
-    def tab1():
-        ct1 = Canvas(root, bg='orange', width=2000, height=1000)
-        ct1.place(x=1, y=1)
-        q = Label(ct1, image=photo)
-        q.pack()
-        label1 = Label(root, text='How to Play', bg='orange' ,font=('Arial', 30))
-        label1.place(x=450, y=100)
-        tab()
-    def tab2():
-        ct2 = Canvas(root, bg='pink', width=2000, height=1000)
-        ct2.place(x=1, y=1)
-        e = Label(ct2, image=photo)
-        e.pack()
-        label2 = Label(root, text='Settings', bg='pink' ,font=('Arial', 30))
-        label2.place(x=450, y=100)
-        tab()
-    c1 = Canvas(root, bg='tan', width=2000, height=100)
-    c1.place(x=1, y=550)
-    tab1_b = Button(root, text='How to Play', font=('Arial', 20), command=tab1)
-    tab1_b.place(x=400, y=567)
-    tab2_b = Button(root, text='Settings', font=('Arial', 20), command=tab2)
-    tab2_b.place(x=800, y=567)
-    tab3_b = Button(root, text='Start Button', font=('Arial', 20), command=pygame_start)
-    tab3_b.place(x=1120, y=567)
-tab()
+LARGEFONT =("Verdana", 175)
+class tkinterApp(tk.Tk):
+	# __init__ function for class tkinterApp 
+	def __init__(self, *args, **kwargs): 
+		
+		# __init__ function for class Tk
+		tk.Tk.__init__(self, *args, **kwargs)
+		
+		# creating a container
+		container = tk.Frame(self) 
+		container.pack(side = "top", fill = "both", expand = True) 
 
-root.mainloop()
+		container.grid_rowconfigure(0, weight = 20)
+		container.grid_columnconfigure(0, weight = 20)
 
-pygame.init()
+		# initializing frames to an empty array
+		self.frames = {} 
 
-wind=pygame.display.set_mode((750,650))
-pygame.display.set_caption("Paint Rush")
-while True:
-         for eve in pygame.event.get():
-              if eve.type==pygame.QUIT:
-                   pygame.quit()
-                   sys.exit() 
+		# iterating through a tuple consisting
+		# of the different page layouts
+		for F in (StartPage, Page1, Page2):
+
+			frame = F(container, self)
+
+			# initializing frame of that object from
+			# startpage, page1, page2 respectively with 
+			# for loop
+			self.frames[F] = frame 
+
+			frame.grid(row = 0, column = 0, sticky ="nsew")
+
+		self.show_frame(StartPage)
+
+	# to display the current frame passed as
+	# parameter
+	def show_frame(self, cont):
+		frame = self.frames[cont]
+		frame.tkraise()
+
+# first window frame startpage
+
+class StartPage(tk.Frame):
+	def __init__(self, parent, controller): 
+		tk.Frame.__init__(self, parent)
+		
+		# label of frame Layout 2
+		label = ttk.Label(self, text ="Paint Rush", font = LARGEFONT)
+		
+		# putting the grid in its place by using
+		# grid
+		label.grid(row = 0, column = 4, padx = 10, pady = 10) 
+
+		button1 = ttk.Button(self, text ="How to Play",
+		command = lambda : controller.show_frame(Page1))
+	
+		# putting the button in its place by
+		# using grid
+		button1.grid(row = 1, column = 1, padx = 10, pady = 10)
+
+		## button to show frame 2 with text layout2
+		button2 = ttk.Button(self, text ="Settings",
+		command = lambda : controller.show_frame(Page2))
+	
+		# putting the button in its place by
+		# using grid
+		button2.grid(row = 2, column = 1, padx = 10, pady = 10)
+
+        # button to show frame 3 with text
+		# layout3
+		button3 = ttk.Button(self, text ="Play game",
+							command = lambda : controller.show_frame(StartPage))
+	
+		# putting the button in its place by
+		# using grid
+		button3.grid(row = 3, column = 1, padx = 10, pady = 10)
+		
+
+
+# second window frame page1 
+class Page1(tk.Frame):
+	
+	def __init__(self, parent, controller):
+		
+		tk.Frame.__init__(self, parent)
+		label = ttk.Label(self, text ="How to Play", font = LARGEFONT)
+		label.grid(row = 0, column = 4, padx = 10, pady = 10)
+
+		# button to show frame 2 with text
+		# layout2
+		button1 = ttk.Button(self, text ="How to Play",
+							command = lambda : controller.show_frame(StartPage))
+	
+		# putting the button in its place 
+		# by using grid
+		button1.grid(row = 1, column = 1, padx = 10, pady = 10)
+		
+		# button to show frame 2 with text
+		# layout2
+		button2 = ttk.Button(self, text ="Settings",
+							command = lambda : controller.show_frame(Page2))
+	
+		# putting the button in its place by 
+		# using grid
+		button2.grid(row = 2, column = 1, padx = 10, pady = 10)
+        
+        # button to show frame 3 with text
+		# layout3
+		button3 = ttk.Button(self, text ="Play game",
+							command = lambda : controller.show_frame(StartPage))
+	
+		# putting the button in its place by
+		# using grid
+		button3.grid(row = 3, column = 1, padx = 10, pady = 10)
+
+
+
+# third window frame page2
+class Page2(tk.Frame): 
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self, parent)
+		label = ttk.Label(self, text ="Settings", font = LARGEFONT)
+		label.grid(row = 0, column = 4, padx = 10, pady = 10)
+
+		# button to show frame 2 with text
+		# layout2
+		button1 = ttk.Button(self, text ="How to Play",
+							command = lambda : controller.show_frame(Page1))
+	
+		# putting the button in its place by 
+		# using grid
+		button1.grid(row = 1, column = 1, padx = 10, pady = 10)
+
+		# button to show frame 3 with text
+		# layout3
+		button2 = ttk.Button(self, text ="Settings",
+							command = lambda : controller.show_frame(StartPage))
+	
+		# putting the button in its place by
+		# using grid
+		button2.grid(row = 2, column = 1, padx = 10, pady = 10)
+
+        # button to show frame 3 with text
+		# layout3
+		button3 = ttk.Button(self, text ="Play game",
+							command = lambda : controller.show_frame(StartPage))
+	
+		# putting the button in its place by
+		# using grid
+		button3.grid(row = 3, column = 1, padx = 10, pady = 10)
+		
+# third window frame page2
+class Page3(tk.Frame): 
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self, parent)
+		label = ttk.Label(self, text ="Play Game", font = LARGEFONT)
+		label.grid(row = 0, column = 4, padx = 10, pady = 10)
+
+		# button to show frame 3 with text
+		# layout3
+		button1 = ttk.Button(self, text ="Main menu",
+							command = lambda : controller.show_frame(Page1))
+	
+		# putting the button in its place by 
+		# using grid
+		button1.grid(row = 1, column = 1, padx = 10, pady = 10)
+
+		# button to show frame 3 with text
+		# layout3
+		button2 = ttk.Button(self, text ="Settings",
+							command = lambda : controller.show_frame(StartPage))
+	
+		# putting the button in its place by
+		# using grid
+		button2.grid(row = 2, column = 1, padx = 10, pady = 10)
+		
+        # button to show frame 3 with text
+		# layout3
+		button3 = ttk.Button(self, text ="Play game",
+							command = lambda : controller.show_frame(StartPage))
+	
+		# putting the button in its place by
+		# using grid
+		button3.grid(row = 3, column = 1, padx = 10, pady = 10)
+		
+
+
+# Driver Code
+app = tkinterApp()
+app.mainloop()
