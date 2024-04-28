7677949fe459552ca1aa450cedc0ecd2a51ce898
Robles4 <164675622+Robles4@users.noreply.github.com>
2024-04-27 20:39:17 -0500

    4/27/2024

--- Final assignments/Final1.py
+++ Final assignments/Final1.py
@@ -4,7 +4,7 @@ import random
 pygame.init()
 
 WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 600
+WINDOW_HEIGHT = 800
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("MS Paint Challenge")
 active_size = 0
@@ -18,8 +18,8 @@ BLUE = (0, 0, 255)
 
 # Objects to draw
 OBJECTS = ["circle", "square", "triangle", "paddle", "star", "heart", "moon", 
-           "earth", "hunky Hawaiian man", "ur mom", "santa", "snowflake", "rainbow",
-           "a bee", "octagon", "Hawaii island", "a laptop"]
+           "earth", "hunky Hawai3ian man", "santa", "snowflake", "rainbow",
+           "a bee", "octagon", "Hawaii island", "a laptop", "heart"]
 
 
 # Define the main game loop
@@ -32,7 +32,7 @@ def main():
     clock = pygame.time.Clock()
     time_remaining = 1000  # Initial time limit (in seconds)
     current_object = random.choice(OBJECTS)  # Choose a random object to draw
-    selected_color = random.choice([RED, GREEN, BLUE, BLACK,])  # Choose a random color
+    selected_color = random.choice([RED, GREEN, BLUE, BLACK])  # Choose a random color
    
     # Font for displaying text
     font = pygame.font.Font(None, 36)
@@ -65,7 +65,7 @@ def main():
         timer_text = font.render("Time: {}s".format(time_remaining), True, BLACK)
         window.blit(timer_text, (10, 10))
        
-        object_text = font.render("Draw a {}".format(current_object), True, BLACK)
+        object_text = font.render("Draw {}".format(current_object), True, BLACK)
         window.blit(object_text, (10, 50))
        
         color_text = font.render("Color: {}".format(selected_color), True, selected_color)
@@ -78,12 +78,12 @@ def main():
             time_remaining -= 1
         else:
             # Time's up, reset the timer and choose a new object to draw
-            time_remaining = 10000
+            time_remaining = 5000
             current_object = random.choice(OBJECTS)
             canvas.fill(WHITE)
        
         # Cap the frame rate
-        clock.tick(400)
+        clock.tick(1000)
 
 # Run the game
 if __name__ == "__main__":
